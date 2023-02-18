70f3c21218645ed53e4e44eebddab041bbc7a269
Biz Melesse <melesse@kippo.com>
2023-02-18 17:57:22 -0600

    KAL-16: Fix tracking timestamp update logic

--- backend/api/src/main/java/ai/kalico/api/service/utils/LeadServiceHelper.java
+++ backend/api/src/main/java/ai/kalico/api/service/utils/LeadServiceHelper.java
@@ -32,7 +32,7 @@ public class LeadServiceHelper {
     if (tokens.length > 1) {
       String email = tokens[0];
       String campaignId = tokens[1];
-      EmailTrackingEntity entity = null;
+      EmailTrackingEntity entity;
       // Locate the existing tracked emails  by campaign id and ip address. We need to do the lookup
       // using these two parameters so each device can only create a single record even if they're
       // opening multiple emails for the same campaign. This is the case when we are opening the
@@ -40,24 +40,25 @@ public class LeadServiceHelper {
       List<EmailTrackingEntity> trackingEntities = emailTrackingRepo.findByEmailAndCampaignId(email, campaignId);
       if (!trackingEntities.isEmpty()) {
         entity = trackingEntities.get(0);
-        entity.setUpdatedAt(LocalDateTime.now());
 
         // Update number of times opened based on the threshold. The default value is 5 minutes. Only consider
         // multiple opens if more than five minutes has elapsed since the last open
         if (LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) - entity.getUpdatedAt().toEpochSecond(ZoneOffset.UTC)
             >= leadsProps.getOpenRateThreshold()) {
           entity.setNumOpened(entity.getNumOpened() + 1);
+          entity.setUpdatedAt(LocalDateTime.now());
+          emailTrackingRepo.save(entity);
           log.info("Email opened campaignId={}\t email={}", campaignId, email);
         }
+
       } else {
         entity = new EmailTrackingEntity();
         entity.setCampaignId(campaignId);
         entity.setEmail(email);
-      }
-
-      if (ipAddress != null) {
-        entity.setIpAddress(ipAddress);
-        emailTrackingRepo.save(entity);
+        if (ipAddress != null) {
+          entity.setIpAddress(ipAddress);
+          emailTrackingRepo.save(entity);
+        }
       }
     } else {
       log.warn(this.getClass().getSimpleName()+ ".getUserEmailImage: Failed to parse image hash '{}'",
